5e98731a6614fe4910c19674f5a9da92e194b46f
amorozzz <MF-7I6oK>
2022-07-11 12:54:58 +0300

    2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,17 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.SQLException;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 5);
+        userService.saveUser("Yana", "Andersen", (byte) 19);
+        userService.saveUser("Irina", "Cvetkova", (byte) 43);
+        userService.saveUser("Egor", "Tarasov", (byte) 34);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
